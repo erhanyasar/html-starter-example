4127c2d8149cb215928684af613c64eda654d46f
Erhan Yaşar <mail@erhanyasar.com.tr>
2022-10-14 16:08:21 +0300

    adds redirect, some tags, basics of js and redirected HTML page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-// window.location = "yonlenme.html";
+// document.getElementById("app").innerHTML = "erhan";
+
+function yasar() {
+    console.log("yasar");
+}
+
+yasar();
+
+/*
+window.onload = () => {
+    console.log('page is fully loaded');
+
+    // document.getElementById("app").innerHTML = "erhan";
+
+    // window.location = "yonlenme.html";
+
+    console.log("Bu bir konsol metni");
+
+    let array = [1, 2, 3, 5];
+    
+    const yeniArray = array.map(x => x * 5);
+    
+    console.log(yeniArray);
+    
+    const erhan = () => {
+      console.log("Bu Erhan'a ait bir konsol metni");
+    };
+      
+    // erhan();
+};
+*/
